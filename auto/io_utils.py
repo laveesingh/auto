329256d4e14d29ebdc13405c99171f1b8550d1da
laveesingh <lavees96@gmail.com>
2018-05-01 19:14:54 +0530

    modified io_utils Logger with text to speech integration

--- auto/io_utils.py
+++ auto/io_utils.py
@@ -1,21 +1,41 @@
+import pyttsx3
 
 
 class Logger:
 
+    ALERT = '\033[31m'
+    WARN = '\033[33m'
+    INFO = '\033[34m'
+    SUCCESS = '\033[36m'
+    FOCUS = '\033[40m'
+    ENDC = '\033[0m'
+
     def __init__(self, *args, **kwargs):
+        self.say = True if kwargs.get('say') else False
+        if self.say:
+            self.engine = pyttsx3.init()
         pass
 
+    def sayit(self, msg):
+        if self.say:
+            self.engine.say(msg)
+            self.engine.runAndWait()
+
+    def log(self, msg, **kwargs):
+        self.sayit(msg)
+        print(kwargs.get('color') + msg + self.ENDC)
+
     def alert(self, msg):
-        print('\033[31m' + msg + '\033[0m')
+        self.log(msg, color=self.ALERT)
 
     def warn(self, msg):
-        print('\033[33m' + msg + '\033[0m')
+        self.log(msg, color=self.WARN)
 
     def info(self, msg):
-        print('\033[34m' + msg + '\033[0m')
+        self.log(msg, color=self.INFO)
 
     def success(self, msg):
-        print('\033[36m' + msg + '\033[0m')
+        self.log(msg, color=self.SUCCESS)
 
     def focus(self, msg):
-        print('\033[40m' + msg + '\033[0m')
+        self.log(msg, color=self.FOCUS)
--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
-from . import main
+#! /usr/bin/python3
+
+from auto import io_utils
 
 
-def test_main():
-    assert main.main() == 0
+def test_logger():
+    logger = io_utils.Logger(say=True)
+    logger.alert('The quick brown fox jumped over a lazy dog')
+    logger.success('We are successful Lavee!')
+    assert True
